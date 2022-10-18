88e529cf8f1538b7aee861816a51a3db4d49c602
Sungmin Lee <sungminlee417@gmail.com>
2022-10-17 19:47:50 -0700

    added sequelize and express boilerplate

---
+++ backend/routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { route } = require("../app");
+const router = express.Router();
+const apiRouter = require("./api");
+
+router.use("/api", apiRouter);
+
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
+  });
+});
+
+module.exports = router;
