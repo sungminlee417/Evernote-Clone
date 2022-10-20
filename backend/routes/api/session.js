54f664bdef365a3cc59430add1a67d0ec59e31c7
Sungmin Lee <sungminlee417@gmail.com>
2022-10-20 11:14:04 -0700

    Add User input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -2,8 +2,24 @@ const express = require("express");
 
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
+
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
 const router = express.Router();
 
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+
+// GET CURRENT USER
 router.get("/", restoreUser, (req, res) => {
   const { user } = req;
   if (user) {
@@ -13,7 +29,8 @@ router.get("/", restoreUser, (req, res) => {
   } else return res.json({});
 });
 
-router.post("/", async (req, res, next) => {
+// LOG IN ROUTE
+router.post("/", validateLogin, async (req, res, next) => {
   const { credential, password } = req.body;
 
   const user = await User.login({ credential, password });
@@ -31,6 +48,7 @@ router.post("/", async (req, res, next) => {
   return res.json({ user });
 });
 
+// LOG OUT
 router.delete("/", (_req, res) => {
   res.clearCookie("token");
   return res.json({ message: "success" });
