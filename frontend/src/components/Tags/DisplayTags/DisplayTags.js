ecf3e1cbe61c63ca85d191fb0b25508f985718e1
Sungmin Lee <sungminlee417@gmail.com>
2022-12-19 13:48:27 -0800

    fixed tags

--- frontend/src/components/Tags/DisplayTags/DisplayTags.js
+++ frontend/src/components/Tags/DisplayTags/DisplayTags.js
@@ -1,27 +1,27 @@
-import "./DisplayTags.css"
+import "./DisplayTags.css";
 import { loadTagsThunk } from "../../../store/tags";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
 const DisplayTags = () => {
-    const dispatch = useDispatch();
-    const tags = Object.values(useSelector((state) => state.tags));
-    const subcontainer = document.querySelector(
-        ".tags-subcontainer"
-      );
-    useEffect(() => {
-        dispatch(loadTagsThunk());
-      }, [dispatch]);
+  const dispatch = useDispatch();
+  const tags = Object.values(useSelector((state) => state.tags));
 
-    if (tags.length === 0) {
-        subcontainer.classList.add("no-tags");
-      } else {
-        subcontainer.classList.add("tags");
-      }
-      
-    return (
-        <div className="tags-subcontainer"></div>
-    );
+  useEffect(() => {
+    dispatch(loadTagsThunk());
+  }, [dispatch]);
+
+  useEffect(() => {
+    const subContainer = document.querySelector(".tags-subcontainer");
+
+    if (tags.length === 0 && subContainer) {
+      subContainer.classList.add("no-tags");
+    } else {
+      subContainer.classList.add("tags");
+    }
+  }, [tags]);
+
+  return <div className="tags-subcontainer"></div>;
 };
 
 export default DisplayTags;
