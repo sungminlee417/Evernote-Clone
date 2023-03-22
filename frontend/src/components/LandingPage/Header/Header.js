7e702a6adb177ebd8b32540a5e2d55f5cd7511bb
Sungmin Lee <sungminlee417@gmail.com>
2023-03-21 17:12:49 -0700

    tailwind refactoring for header landing page

--- frontend/src/components/LandingPage/Header/Header.js
+++ frontend/src/components/LandingPage/Header/Header.js
@@ -1,6 +1,4 @@
-import "./Header.css";
 import { NavLink } from "react-router-dom";
-import logo from "../../../images/EvernoteLogo.svg";
 import { useState, useEffect } from "react";
 
 const Header = () => {
@@ -26,22 +24,26 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed w-full flex justify-between items-center tracking-wide bg-white transition-all duration-200 lg:px-32 md:px-20 px-8 py-10 ${
+      className={`fixed w-full flex justify-between items-center tracking-wide bg-white transition-all duration-200 lg:px-32 md:px-20 px-6 py-8 ${
         scroll ? "shadow-md" : ""
       }`}
     >
       <button
         to="/"
-        className="home-button"
+        className="bg-transparent border-none cursor-pointer flex items-center gap-4"
         onClick={() => scrollSmoothlyTo("landing-page")}
       >
-        <img src={logo} alt="evernote_logo" />
+        <i class="fa-brands fa-evernote text-[#00a82d] text-7xl"></i>
+        <strong className="tracking-wide text-4xl">Betternote</strong>
       </button>
-      <div className="landing-page-header-right">
+      <div className="flex items-center gap-4">
         <a href="https://github.com/sungminlee417/Evernote-Clone.git">
-          <i className="fa-brands fa-github landing-page-github"></i>
+          <i className="fa-brands fa-github cursor-pointer text-4xl"></i>
         </a>
-        <NavLink to="/login" className="login-button">
+        <NavLink
+          to="/login"
+          className="cursor-pointer text-3xl text-black hover:text-[#00a82d] transition ease-in-out duration-200"
+        >
           Log In
         </NavLink>
       </div>
