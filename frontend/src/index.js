fe07bd4b68888898c2ed3f3c7f89eb24dab79068
Sungmin Lee <sungminlee417@gmail.com>
2023-03-22 13:24:22 -0700

    deleted unecessary code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,25 +10,9 @@ import App from "./App";
 
 import configureStore from "./store";
 
-import { restoreCSRF, csrfFetch } from "./store/csrf";
-
-import * as sessionActions from "./store/session";
 import { TagProvider } from "./components/context/TagContext";
-import { CurrentNotebookProvider } from "./components/context/CurrentNotebook";
 
 const store = configureStore();
-
-if (process.env.NODE_ENV !== "production") {
-  restoreCSRF();
-
-  window.csrfFetch = csrfFetch;
-  window.store = store;
-  window.sessionActions = sessionActions;
-}
-
-if (process.env.NODE_ENV !== "production") {
-  window.store = store;
-}
 
 function Root() {
   return (
